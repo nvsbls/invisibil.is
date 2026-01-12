8e498e47735368a6f3609d5f8f2f037ffa4f83bf
consistncy <jam@invisibilis.xyz>
2026-01-12 20:00:56 +0100

    v1

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="m-2">
+      <h1 className="font-bold mb-4">invisibilis</h1>
+      <div className="flex gap-4">
+        <a href="https://www.are.na/group/invisibilis/invite/PxGbN_v7LJI1a85VOYXwug">research</a>
+        <a href="https://discord.com/invite/m28JKSR6m5">community</a>
+      </div>
+    </div>
+  );
+}
